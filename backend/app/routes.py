09387834bad43747655dc7cda960229cd2fa2053
Kevin Gil <kevin@sellscale.com>
2025-08-02 11:17:20 -0700

    add agent services

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,7 @@
 from app.user.controllers import users
+from app.chat.controllers import chat
 
 
 def register_routes(app):
     app.register_blueprint(users)
+    app.register_blueprint(chat, url_prefix="/api/chat")

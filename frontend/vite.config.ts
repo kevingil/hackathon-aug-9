ded5d1edaf7a11b816c61cab723c712d7186a929
Kevin Gil <kevin@sellscale.com>
2025-08-02 18:05:13 -0700

    update vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { BASE_URL } from "./src/api/url";
+import { BASE_URL } from "./src/api/const";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
